9df2cee4caffda3e14bf35592f2bb78e16c35912
joaoaraujo2003 <joaopda15@gmail.com>
2025-12-30 12:45:55 +0000

    Foi adicionada a API KEY ao sensor

--- sensor.py
+++ sensor.py
@@ -3,7 +3,13 @@ import time
 import random
 
 API_URL = "https://localhost:44331/api/sensores/movimento"
+API_KEY = "ISI-2025-SECRET"   # 🔑 a tua chave
 SALA_ID = 1
+
+headers = {
+    "X-API-KEY": API_KEY,
+    "Content-Type": "application/json"
+}
 
 while True:
     movimento = random.choice([True, True, True, False])
@@ -14,8 +20,21 @@ while True:
     }
 
     try:
-        r = requests.post(API_URL, json=payload, verify=False)
-        print(f"[SALA {SALA_ID}] Movimento:", movimento)
+        r = requests.post(
+            API_URL,
+            json=payload,
+            headers=headers,
+            verify=False
+        )
+
+        print(
+            f"[SALA {SALA_ID}] Movimento: {movimento} | "
+            f"Status: {r.status_code}"
+        )
+
+        if r.status_code != 200:
+            print("Resposta da API:", r.text)
+
     except Exception as e:
         print("Erro:", e)
 
